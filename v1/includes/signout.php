3f5ba0e3eada2b075aae22d45b58c9307f6fa4b7
vikingeff <mail.leger@gmail.com>
2015-01-27 14:33:40 +0100

    remove doubles

--- v1/includes/signout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['loggued_on_user']);
-	session_destroy();
-	header("Location: ../index.php");
-	die();
-	exit("OK\n");
-?>
--- v2/client.php
+++
@@ -1,4 +0,0 @@
-<?php include("includes/header.php"); ?>
-<h1>Client</h1>
-<p>Voir organisation de la page client<br/><br/></p>
-<?php include("includes/footer.php"); ?>
